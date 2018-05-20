813ed27e68e4f2db7b908a4972793b1d29621384
JohnFunkCode <jfunk@pivotal.io>
2018-05-20 08:51:31 -0600

    additions for Cloud Foundry

--- run_app.py
+++ run_app.py
@@ -4,6 +4,7 @@ from library.flask_pi_iot_app import app
 if __name__ == '__main__':
     app.debug = True
 #    host = os.environ.get('IP','127.0.0.1')
-    host = os.environ.get('IP', '192.168.137.179')
-    port = int( os.environ.get('Port', 8080))
+#    host = os.environ.get('IP','10.10.10.14')
+    host = os.environ.get('IP','0.0.0.0') #cloud foundry uses 0.0.0.0 to listen to all IPs
+    port = int( os.environ.get('Port',8080))
     app.run(host=host, port=port)
