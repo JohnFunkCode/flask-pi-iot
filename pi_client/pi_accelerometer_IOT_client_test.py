081cee2057d0229bbed3b68195294cba5ebf348a
JohnFunkCode <jfunk@pivotal.io>
2018-05-26 09:56:45 -0600

    First cut of more robust client code

--- pi_client/pi_accelerometer_IOT_client_test.py
+++ pi_client/pi_accelerometer_IOT_client_test.py
@@ -1,7 +1,7 @@
 # Import the ADXL345 module.
 
 import unittest
-import import pi_accelerometer_IOT_client as piac
+import pi_accelerometer_IOT_client as piac
 
 class test_PiAccelerometerIOTClient(unittest.TestCase):
 
@@ -12,6 +12,12 @@ class test_PiAccelerometerIOTClient(unittest.TestCase):
         apic = piac.PiAccelerometerIOTClient()
         serial=apic.getserial()
         self.assertTrue(len(serial)>1)
+
+    def test_read_accelerometer(self):
+        x, y, z = self._accel.read()
+        self.assertTrue(x!=0)
+        self.assertTrue(y!=0)
+        self.assertTrue(z!=0)
 
 if __name__ == '__main__':
     unittest.main()
