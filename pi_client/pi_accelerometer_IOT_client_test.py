a3b9ffecd178bc7b17f086e541cc6d502ffa4c37
JohnFunkCode <jfunk@pivotal.io>
2018-05-26 09:27:02 -0600

    Test for pi_accelerometer_IOT_client

---
+++ pi_client/pi_accelerometer_IOT_client_test.py
@@ -0,0 +1,16 @@
+# Import the ADXL345 module.
+
+import unittest
+import pi_accelerometer_IOT_client
+
+class test_PiAccelerometerIOTClient(unittest.TestCase):
+
+    def setUp(self):
+        return
+
+    def test_getserial(self):
+        serial=pi_accelarometer.getserial()
+        self.assertTrue(len(serial)>1)
+
+if __name__ == '__main__':
+    unittest.main()
