d4cdaee6087c9f03f633af79ddbd493836a2972d
JohnFunkCode <jfunk@pivotal.io>
2018-05-27 09:14:03 -0600

    Added another server

--- pi_client/pi_accelerometer_IOT_client.py
+++ pi_client/pi_accelerometer_IOT_client.py
@@ -9,7 +9,7 @@ class PiAccelerometerIOTClient:
 
     _accel = Adafruit_ADXL345.ADXL345()
     _serial = ''
-    _server_destinations = ['http://jpf-flask-pi-iot.cfapps.io/test','http://10.10.10.14/test']
+    _server_destinations = ['http://jpf-flask-pi-iot.cfapps.io/test','http://10.10.10.14:8080/test']
 
     def getserial(self):
       # Extract serial from cpuinfo file
@@ -52,7 +52,7 @@ class PiAccelerometerIOTClient:
     def __init__(self):
         _serial = self.getserial()
         _accel = Adafruit_ADXL345.ADXL345()
-        _server_destinations = ['http://jpf-flask-pi-iot.cfapps.io/test','http://10.10.10.14/test']
+        _server_destinations = ['http://jpf-flask-pi-iot.cfapps.io/test','http://10.10.10.14:8080/test']
 
 
 if __name__ == "__main__":
