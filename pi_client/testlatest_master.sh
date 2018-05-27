c418551da91da0cf664c9a9c8eef0fb653c3954a
JohnFunkCode <jfunk@pivotal.io>
2018-05-27 09:37:10 -0600

    created versions to get either master and development branches onto the Raspberry Pi

---
+++ pi_client/testlatest_master.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo *** getting lastest from master ****
+echo pi_accelerometer_IOT_client_test
+curl -OL https://raw.githubusercontent.com/JohnFunkCode/flask-pi-iot/master/pi_client/pi_accelerometer_IOT_client_test.py > pi_accelerometer_IOT_client_test.py
+echo pi_accelerometer_IOT_client
+curl -OL https://raw.githubusercontent.com/JohnFunkCode/flask-pi-iot/master/pi_client/pi_accelerometer_IOT_client.py > pi_accelerometer_IOT_client.py
+python pi_accelerometer_IOT_client_test.py -v
