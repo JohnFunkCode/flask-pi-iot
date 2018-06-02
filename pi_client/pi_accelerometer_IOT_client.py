b974e1f892b0821701ac48a683d5629b101a57bc
JohnFunkCode <jfunk@pivotal.io>
2018-06-02 16:55:27 -0600

    Refresh the servers we post to every 100 readings

--- pi_client/pi_accelerometer_IOT_client.py
+++ pi_client/pi_accelerometer_IOT_client.py
@@ -78,7 +78,7 @@ class PiAccelerometerIOTClient:
         while(keep_posting==True):
             # get the data
             number_of_readings+=1
-            if(number_of_readings>99)
+            if(number_of_readings > 99):
                 keep_posting=False
             x,y,z=self._accel.read()
             #format it to send to server
