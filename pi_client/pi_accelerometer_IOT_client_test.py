69531f88de0194a0ed6c3c5dcbda78a8120dd252
JohnFunkCode <jfunk@pivotal.io>
2018-06-02 16:27:06 -0600

    Refine tests

--- pi_client/pi_accelerometer_IOT_client_test.py
+++ pi_client/pi_accelerometer_IOT_client_test.py
@@ -10,39 +10,47 @@ class test_PiAccelerometerIOTClient(unittest.TestCase):
 
     def test_getserial(self):
         '''simple test to make sure the serial number isn't empty'''
+        print("**test_getserial")
         apic = piac.PiAccelerometerIOTClient()
         serial=apic.getserial()
         self.assertTrue(len(serial)>1)
 
     def test_get_server_destinations(self):
         '''simple test to make sure the list of server destinations isn't empty'''
+        print("**test_get_server_destinations")
         apic = piac.PiAccelerometerIOTClient()
         serial=apic.get_server_destinations()
         self.assertTrue(len(serial)>1)
 
     def test_print_server_destinations(self):
+        print("**test_print_server_destinations")
         apic = piac.PiAccelerometerIOTClient()
         apic.print_server_destinations()
 
     def test_print_valid_server_destinations(self):
+        print("**test_print_valid_server_destinations")
         apic = piac.PiAccelerometerIOTClient()
         apic.print_valid_server_destinations()
 
     def test_get_valid_server_destinations(self):
+        print("**test_get_valid_server_destinations")
         apic = piac.PiAccelerometerIOTClient()
         vsd=apic.get_valid_server_destinations()
         self.assertTrue(len(vsd)>0)
 
     def test_is_server_available(self):
+        print("**test_is_server_available")
         apic = piac.PiAccelerometerIOTClient()
         self.assertFalse(apic.is_server_available("http://99.99.99.99:99"))
         self.assertTrue(apic.is_server_available("http://jpf-flask-pi-iot/test"))
 
     def test_print_invalid_server_destinations(self):
+        print("**test_print_invalid_server_destinations")
         apic = piac.PiAccelerometerIOTClient()
         apic.print_invalid_server_destinations()
 
     def test_read_accelerometer(self):
+        print("**test_read_accelerometer")
         '''simple test to make sure it doesn't throw an exception'''
         apic = piac.PiAccelerometerIOTClient()
         x, y, z = apic._accel.read()
