7bc12f2214bfb11b128b74731ca858be3b4fb508
JohnFunkCode <jfunk@pivotal.io>
2018-06-02 10:39:32 -0600

    make the posting stop if a server returns an error

--- pi_client/pi_accelerometer_IOT_client.py
+++ pi_client/pi_accelerometer_IOT_client.py
@@ -85,9 +85,8 @@ class PiAccelerometerIOTClient:
                 try:
                     print("  Posting to {0}".format(server))
                     r=requests.post(server,data=aData)
-                except:
-                    requests.exceptions.RequestException:
-                        print("   Server: {0} raises an exception".format(server))
+                except requests.exceptions.RequestException:
+                    print("   Server: {0} raises an exception".format(server))
                 if(r.status_code!=200):
                     break
             if(r.status_code!=200):
